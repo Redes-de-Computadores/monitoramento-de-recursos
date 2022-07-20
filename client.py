0eab1d4481a4dbb3ebaaba0f5aab387173367cca
Marcus <marcusbarbosa.aluno@unipampa.edu.br>
2022-07-20 15:11:58 -0300

    updates

--- client.py
+++ client.py
@@ -1,14 +1,29 @@
 import socket
 
+from numpy import full
+
+HEADERSIZE = 10
+
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soc.connect((socket.gethostname(), 1234))
 
-full_msg = ''
 
 while True:
-    msg = soc.recv(1024)
-    if len(msg) <=0 :
-        break 
-    full_msg += msg.decode("utf-8")
-    
-print(full_msg)
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = soc.recv(16)
+        if new_msg:
+            print(f"new message lenght: {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg rcvd!")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
+        
+    print(full_msg)
--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 import socket
 
+HEADERSIZE = 10
+
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc.bind((socket.gethostname(), 1234))
+soc.bind((socket.gethostname(), 1235))
 soc.listen(5)
 
 while True: 
     clientsocket, adress = soc.accept()
     print(f"Connection from {adress} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-    clientsocket.send(bytes("\nHello Wrld! -socket", "utf-8"))
-    clientsocket.close()
+
+    msg = "Welcome to the server!"
+    msg = f'{len(msg):<{HEADERSIZE}}' + msg
+
+    clientsocket.send(bytes(msg, "utf-8"))
